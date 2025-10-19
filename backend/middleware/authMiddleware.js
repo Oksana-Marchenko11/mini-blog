b0d4a61b0c6a88339b2fb07bfe4b11c484cd6c78
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2025-10-19 23:19:04 +0300

    fixed token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,19 @@
 import { User } from "../models/user.js";
+import jwt from "jsonwebtoken";
+
 export async function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
+  console.log("AuthHeader: ", authHeader);
   if (!authHeader) return res.status(401).json({ error: "No token provided" });
 
   const token = authHeader.split(" ")[1];
+  console.log("token: ", token);
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded._id);
-    console.log(user);
+    console.log(decoded);
+    const user = await User.findById(decoded.id);
+    console.log("user: ", user);
     if (!user) return res.status(401).json({ error: "User not found" });
 
     req.user = user;
