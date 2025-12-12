7293dfe76b6dea6c0bf938cd45d3d3c2177023b7
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2025-12-12 23:57:53 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { Container, Form, Button, Alert } from "react-bootstrap";
 import { API_BASE } from "../config.js";
 
 const RegisterPage = () => {
+  const [error, setError] = useState("");
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -12,6 +15,18 @@ const RegisterPage = () => {
       username: form.username.value,
       password: form.password.value,
     };
+       const confirm = form.confirm.value;
+     if (formData.password.length < 6) {
+      return setError("Пароль має містити мінімум 6 символів");
+    }
+
+    if (!/[0-9]/.test(formData.password)) {
+      return setError("Пароль має містити хоча б одну цифру");
+    }
+
+    if (formData.password !== confirm) {
+      return setError("Паролі не збігаються");
+    }
     console.log("Register data:", formData);
     console.log(JSON.stringify(formData));
     fetch(`${API_BASE}/api/auth/register`, {
@@ -35,6 +50,8 @@ const RegisterPage = () => {
   return (
     <Container className="my_container">
       <h2 className="mb-4">Реєстрація</h2>
+
+      {error && <Alert variant="danger">{error}</Alert>}
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label>Email</Form.Label>
@@ -63,7 +80,17 @@ const RegisterPage = () => {
             required
           />
         </Form.Group>
-        <Button variant="success" type="submit" className="w-100 mb-2">
+
+        <Form.Group className="mb-3">
+          <Form.Label>Підтвердження пароля</Form.Label>
+          <Form.Control
+          type="password"
+          name="confirm"
+          placeholder="Пароль"
+          required />
+        </Form.Group>
+
+         <Button variant="success" type="submit" className="w-100 mb-2">
           Зареєструватися
         </Button>
       </Form>
